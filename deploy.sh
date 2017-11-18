f9143180128c18fa9a634967d4e34e0351e57ec2
Patrick Titzler <ptitzler@us.ibm.com>
2017-11-17 17:38:21 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+# verify that the required Cloud Foundry variables are set
+# - BXIAM: IBM Cloud API key
+if [ -z ${BXIAM+x} ]; then echo 'Environment variable BXIAM is undefined.'; return 1; fi
+# - CF_ORGANIZATION: IBM Cloud/Cloud Foundry organization name
+if [ -z ${CF_ORGANIZATION+x} ]; then echo 'Environment variable CF_ORGANIZATION is undefined.'; return 1; fi
+# - CF_SPACE: IBM Cloud/Cluod Foundry space name
+if [ -z ${CF_SPACE+x} ]; then echo 'Environment variable CF_SPACE is undefined.'; return 1; fi
+# - APP_NAME: IBM Cloud/Cluod Foundry application name
+if [ -z ${APP_NAME+x} ]; then echo 'Environment variable APP_NAME is undefined.'; return 1; fi
+
+# set optional Cloud Foundry variables if they are not set
+# - CF_API: IBM Cloud API endpoint (default to US-South region)
+if [ -z ${CF_API+x} ]; then export CF_API='https://api.ng.bluemix.net'; fi
+
+# verify that the required application specific variables are set
+# - refer to documentation
+if [ -z ${CLOUDANT_URL+x} ]; then echo 'Environment variable CLOUDANT_URL is undefined.'; return 1; fi
+if [ -z ${CLOUDANT_DB+x} ]; then echo 'Environment variable CLOUDANT_DB is undefined.'; return 1; fi
+if [ -z ${SLACK_TOKEN+x} ]; then echo 'Environment variable SLACK_TOKEN is undefined.'; return 1; fi
+
 # login and set target
 ./Bluemix_CLI/bin/bluemix config --check-version false
-./Bluemix_CLI/bin/bluemix api https://api.ng.bluemix.net
+./Bluemix_CLI/bin/bluemix api $CF_API
 ./Bluemix_CLI/bin/bluemix login --apikey $BXIAM
 ./Bluemix_CLI/bin/bluemix target -o $CF_ORGANIZATION -s $CF_SPACE
 # push app but don't start it (app start will fail unless the following environment
